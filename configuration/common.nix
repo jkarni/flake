c56c9b0e3e770fec02828ad7b66e9975214712e9
mlyxshi <mlyxdev@gmail.com>
2022-05-16 03:56:24 -0700

    fix

--- configuration/common.nix
+++ configuration/common.nix
@@ -16,7 +16,7 @@
 
   users.defaultUserShell = pkgs.zsh;
   users.users.root.hashedPassword = "$6$fwJZwHNLE640VkQd$SrYMjayP9fofIncuz3ehVLpfwGlpUj0NFZSssSy8GcIXIbDKI4JnrgfMZxSw5vxPkXkAEL/ktm3UZOyPMzA.p0";
-
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe"];
 
   time.timeZone = "America/Los_Angeles";
 
--- service/openssh.nix
+++ service/openssh.nix
@@ -12,8 +12,4 @@
     permitRootLogin = "yes";
   };
 
-  
-  users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe"];
-
-
 }
