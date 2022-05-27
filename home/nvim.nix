5955f0ed21879105a769114bbdad25f601bd63cd
mlyxshi <mlyxdev@gmail.com>
2022-05-26 19:04:28 -0700

    final fix nvim

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,17 +1,20 @@
- {pkgs, ... }@args: {
-  xdg.configFile."nvim".source = ../config/nvim;
+{ pkgs, ... }@args: {
+
+  # Do not include Packer's dir -- nvim/plugin
+  xdg.configFile."nvim/init.lua".source = ../config/nvim/init.lua;
+  xdg.configFile."nvim/lua".source = ../config/nvim/lua;
 
   home.packages = with pkgs;  [
-    
-      neovim
-      sumneko-lua-language-server #Lua
-      cargo #Rust
-      clang #C,C++
-      clang-tools #C,C++
-      nodejs # LSP Installer
 
-      # Telescope
-      fd
-      ripgrep
+    neovim
+    sumneko-lua-language-server #Lua
+    cargo #Rust
+    clang #C,C++
+    clang-tools #C,C++
+    nodejs # LSP Installer
+
+    # Telescope
+    fd
+    ripgrep
   ];
 }
