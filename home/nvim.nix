3dc8517eb89c2522f40cfb845a5da1ceb25b0a97
mlyxshi <mail@mlyxshi.com>
2022-05-26 17:56:07 -0700

    fix

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,34 +1,9 @@
  {pkgs, ... }@args: {
+  xdg.configFile."nvim".source = ../config/nvim/;
 
-  xdg.configFile."nvim/lua".source = ../config/nvim/lua;
-
-
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-
-      (nvim-treesitter.withPlugins
-        (
-          plugins: with plugins; [
-            #tree-sitter-nix
-            tree-sitter-bash
-            tree-sitter-json
-            tree-sitter-typescript
-            tree-sitter-c
-            tree-sitter-cpp
-            tree-sitter-lua
-            tree-sitter-rust
-            tree-sitter-python
-            tree-sitter-markdown
-          ]
-        )
-      )
-
-
-    ];
-
-
-    extraPackages = with pkgs; [
+  home.packages = with pkgs;  [
+    
+      neovim
       sumneko-lua-language-server #Lua
       cargo #Rust
       clang #C,C++
@@ -38,50 +13,5 @@
       # Telescope
       fd
       ripgrep
-    ];
-
-
-
-    # homemanager current not support init.lua 
-    # workaround
-    # add extraConfig to  ~/.config/nvim/init.vim
-    extraConfig = "
-      lua require('options')
-      lua require('plugins')
-      lua require('colorscheme')
-      lua require('keybindings')
-      lua require('autocmds')
- 
- 
-      lua require('lsp')
-      lua require('lsp.ui')
-      lua require('lsp.cmp')
-
-      lua require('plugin-config.bufferline')
-      lua require('plugin-config.feline')
-      lua require('plugin-config.nvim-tree')
-      lua require('plugin-config.telescope')
-      lua require('plugin-config.fidget')
-      lua require('plugin-config.alpha')
-      lua require('plugin-config.project')
-      lua require('plugin-config.nvim-treesitter')
-      lua require('plugin-config.indent-blankline')
-      lua require('plugin-config.autopair')
-      lua require('plugin-config.modes')
-      lua require('plugin-config.comment')
-      lua require('plugin-config.gitsigns')
-      lua require('plugin-config.toggleterm')
-      lua require('plugin-config.whichkey')
-      lua require('plugin-config.gps')
-      lua require('plugin-config.autosession')
-      lua require('plugin-config.reverse')
-
-
-      lua require('plugin-config.colorizer')    
-   ";
-
-
-  };
-
-
+  ];
 }
