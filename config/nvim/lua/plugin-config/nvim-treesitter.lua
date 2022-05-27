38e1ca49db5d4d304a998524ceb2b260eb31182e
mlyxshi <mlyxdev@gmail.com>
2022-05-26 18:57:13 -0700

    final fix nvim

--- config/nvim/lua/plugin-config/nvim-treesitter.lua
+++ config/nvim/lua/plugin-config/nvim-treesitter.lua
@@ -4,13 +4,17 @@ if not status then
   return
 end
 
+--Very Strange Fix! https://github.com/nvim-treesitter/nvim-treesitter/issues/1449
+--But it just works in Both MacOS and NixOS!
+-- The root issue of clang in NixOS:
+--https://github.com/nvim-treesitter/nvim-treesitter/issues/1449#issuecomment-870482532
+
 require 'nvim-treesitter.install'.compilers = { 'clang++' }
 
 treesitter.setup({
-  -- 安装 language parser
-  -- :TSInstallInfo 命令查看支持的语言
+
   ensure_installed = require("lang-config.treesitter.parsers"),
-  -- 启用代码高亮模块
+
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
@@ -21,3 +25,4 @@ treesitter.setup({
   },
 
 })
+
