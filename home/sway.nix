5583cf7915c423b39f017222916c63b5055ac2a0
mlyxshi <mlyxdev@gmail.com>
2022-05-28 04:26:14 -0700

    fix

--- home/sway.nix
+++ home/sway.nix
@@ -3,6 +3,10 @@
     ./common.nix
   ];
 
+  # Apple Keyboard
+  # control->control
+  # option->Alt_L->Mod1
+  # command->Super_L->Mod4
   xdg.configFile."sway/config".source = ../config/sway/config;
   xdg.configFile."waybar/config".source = ../config/waybar/config;
   xdg.configFile."foot/foot.ini".source = ../config/foot/foot.ini;
@@ -15,4 +19,21 @@
     size = 24;
     gtk.enable = true;
   };
+
+
+  home.packages = with pkgs; [
+    swaylock
+    swayidle
+    wl-clipboard
+    mako
+    waybar
+    foot
+    pavucontrol
+    bemenu
+    mpv
+    baobab
+    firefox-wayland
+
+    xorg.xlsclients #debug xorg.xev xorg.xmodmap 
+  ];
 }
--- service/sway.nix
+++ service/sway.nix
@@ -3,30 +3,9 @@
     ./common.nix
   ];
 
-  # Apple Keyboard
-  # control->control
-  # option->Alt_L->Mod1
-  # command->Super_L->Mod4
-
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-
-      swaylock
-      swayidle
-      wl-clipboard
-      mako
-      waybar
-      foot
-      pavucontrol
-      bemenu
-      mpv
-      baobab
-      firefox-wayland
-
-      xorg.xlsclients #debug xorg.xev xorg.xmodmap 
-    ];
   };
 
   environment.loginShellInit = ''
