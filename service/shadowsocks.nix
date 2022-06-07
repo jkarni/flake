abb9107ceb9612fd89a6f56e8b386f9f41adc615
mlyxshi <mail@mlyxshi.com>
2022-06-06 21:16:51 -0700

    fix

--- service/shadowsocks.nix
+++ service/shadowsocks.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     shadowsocks-rust
   ];
@@ -9,8 +9,8 @@
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-        Restart = "always";
-        ExecStart = "${pkgs.shadowsocks-rust}/bin/ssserver -s 0.0.0.0 -p 6666 -m chacha20-ietf-poly1305 -k 12345 -u --fast-open";
+      Restart = "always";
+      ExecStart = "${pkgs.shadowsocks-rust}/bin/ssserver -s 0.0.0.0 -p 6666 -m chacha20-ietf-poly1305 -k 12345 -u --fast-open";
     };
 
   };
