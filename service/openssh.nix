374a1277af0ecd0a66881c482c8d3064c3acc71e
mlyxshi <mlyxdev@gmail.com>
2022-06-09 19:49:00 -0700

    fix openssh

--- service/openssh.nix
+++ service/openssh.nix
@@ -7,6 +7,14 @@
 
   # ssh (client)
   programs.ssh = {
+
+    knownHosts = {
+      github = {
+        hostNames = [ "github.com" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
+    };
+
     extraConfig = ''
       Host jp1
         HostName jp1.mlyxshi.com
