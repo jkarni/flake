4b3ddfefce60ecc3eb14c69180c58d886b4aefc8
mlyxshi <mail@mlyxshi.com>
2022-06-11 06:46:53 -0700

    todo: grafana

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -2,16 +2,21 @@
   # grafana configuration
   services.grafana = {
     enable = true;
-    domain = "jp2.mlyxshi.com";
+    domain = "localhost";
     port = 2333;
     addr = "127.0.0.1";
   };
 
   # nginx reverse proxy
+  services.nginx.enable = true;
   services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    locations."/grafana/" = {
+    locations."/" = {
       proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
       proxyWebsockets = true;
+      # workaround https://community.grafana.com/t/after-update-to-8-3-5-origin-not-allowed-behind-proxy/60598/7
+      extraConfig = ''
+        proxy_set_header Host jp2.mlyxshi.com;
+      '';
     };
   };
 
