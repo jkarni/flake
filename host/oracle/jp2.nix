cc5c4a2cbb12d79fb3ec2f169a324894160a436c
mlyxshi <mail@mlyxshi.com>
2022-06-11 06:09:42 -0700

    todo: grafana

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -1,9 +1,18 @@
-{
+{ config, pkgs, ... }: {
+  # grafana configuration
   services.grafana = {
     enable = true;
     domain = "jp2.mlyxshi.com";
     port = 2333;
     addr = "127.0.0.1";
+  };
+
+  # nginx reverse proxy
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+      proxyWebsockets = true;
+    };
   };
 
 }
