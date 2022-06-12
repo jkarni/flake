dd579d57cced0ed849413ecf3bc98c5e50469d63
mlyxshi <mail@mlyxshi.com>
2022-06-11 18:10:31 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -20,10 +20,30 @@
     };
   };
 
-  
+
   services.prometheus = {
     enable = true;
     port = 9001;
+
+    scrapeConfigs = [
+      {
+        job_name = "MLYXSHI";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+
+  };
+
+  services.prometheus = {
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
   };
 
 
