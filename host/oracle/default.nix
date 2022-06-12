6a662643b1a7945e92a83873438aa638072a14d3
mlyxshi <mail@mlyxshi.com>
2022-06-11 23:55:32 -0700

    test

--- host/oracle/default.nix
+++ host/oracle/default.nix
@@ -17,34 +17,6 @@
     };
   };
 
-  services.prometheus = {
-    enable = true;
-    port = 9090;
-
-    globalConfig = {
-      scrape_interval = "1m";
-      evaluation_interval = "1m";
-    };
-
-    scrapeConfigs = [
-      {
-        job_name = "prometheus_exporters_systemd";
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
-
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-        port = 9091;
-      };
-    };
-
-  };
-
 
 }
 
--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -1,5 +1,22 @@
 { config, pkgs, ... }: {
 
+ services.prometheus = {
+    enable = true;
+    retentionTime = "7d";
 
+    globalConfig = {
+      scrape_interval = "1m";
+      evaluation_interval = "1m";
+    };
+
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9091;
+      };
+    };
+
+  };
 
 }
