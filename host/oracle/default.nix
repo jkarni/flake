a07373d5d99fa7d49932cc738afc8f7c3f562b44
mlyxshi <mail@mlyxshi.com>
2022-06-11 20:54:39 -0700

    test

--- host/oracle/default.nix
+++ host/oracle/default.nix
@@ -16,5 +16,35 @@
       DHCP = "yes";
     };
   };
+
+  services.prometheus = {
+    enable = true;
+    port = 9090;
+
+    globalConfig = {
+      scrape_interval = "1m";
+      evaluation_interval = "1m";
+    };
+
+    scrapeConfigs = [
+      {
+        job_name = "prometheus_exporters_systemd";
+        static_configs = [{
+          targets = [ "127.0.0.1:${config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9091;
+      };
+    };
+
+  };
+
+
 }
 
--- host/oracle/kr.nix
+++ host/oracle/kr.nix
@@ -2,7 +2,7 @@
   # grafana configuration
   services.grafana = {
     enable = true;
-    domain = "localhost";
+    domain = "kr.mlyxshi.com";
     port = 2333;
     addr = "127.0.0.1";
   };
@@ -11,7 +11,7 @@
   services.nginx.enable = true;
   services.nginx.virtualHosts.${config.services.grafana.domain} = {
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+      proxyPass = "http://127.0.0.1:${config.services.grafana.port}";
       proxyWebsockets = true;
       # workaround https://community.grafana.com/t/after-update-to-8-3-5-origin-not-allowed-behind-proxy/60598/7
       extraConfig = ''
@@ -19,36 +19,6 @@
       '';
     };
   };
-
-
-  services.prometheus = {
-    enable = true;
-    port = 9090;
-
-    globalConfig = {
-      scrape_interval = "1m";
-      evaluation_interval = "1m";
-    };
-
-    scrapeConfigs = [
-      {
-        job_name = "prometheus_exporters_systemd";
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
-
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-        port = 9091;
-      };
-    };
-
-  };
-
 
 
 }
