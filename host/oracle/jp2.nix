cc762c2b78207cf216680abcfe59c0ac59cb23bd
mlyxshi <mail@mlyxshi.com>
2022-06-11 18:42:20 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -1,25 +1,4 @@
 { config, pkgs, ... }: {
-  # grafana configuration
-  services.grafana = {
-    enable = true;
-    domain = "localhost";
-    port = 2333;
-    addr = "127.0.0.1";
-  };
-
-  # nginx reverse proxy
-  services.nginx.enable = true;
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-      proxyWebsockets = true;
-      # workaround https://community.grafana.com/t/after-update-to-8-3-5-origin-not-allowed-behind-proxy/60598/7
-      extraConfig = ''
-        proxy_set_header Host jp2.mlyxshi.com;
-      '';
-    };
-  };
-
 
   services.prometheus = {
     enable = true;
@@ -27,7 +6,7 @@
 
     scrapeConfigs = [
       {
-        job_name = "MLYXSHI";
+        job_name = "prometheus_exporters_systemd";
         static_configs = [{
           targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
         }];
--- host/oracle/kr.nix
+++ host/oracle/kr.nix
@@ -1,6 +1,51 @@
-{
+{ config, pkgs, ... }: {
+  # grafana configuration
+  services.grafana = {
+    enable = true;
+    domain = "localhost";
+    port = 2333;
+    addr = "127.0.0.1";
+  };
+
+  # nginx reverse proxy
+  services.nginx.enable = true;
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+      proxyWebsockets = true;
+      # workaround https://community.grafana.com/t/after-update-to-8-3-5-origin-not-allowed-behind-proxy/60598/7
+      extraConfig = ''
+        proxy_set_header Host kr.mlyxshi.com;
+      '';
+    };
+  };
+
+
   services.prometheus = {
     enable = true;
     port = 9001;
+
+    scrapeConfigs = [
+      {
+        job_name = "prometheus_exporters_systemd";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+
   };
+
+  services.prometheus = {
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+  };
+
+
+
 }
