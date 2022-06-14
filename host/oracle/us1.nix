bebbff07fbe3b08a57fb0504f923632cd08fa02d
mlyxshi <mail@mlyxshi.com>
2022-06-14 04:42:43 -0700

    test smb

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   imports = [
     ./default.nix
   ];
@@ -14,7 +14,7 @@
         comment = "Public samba share.";
       };
     
-    invalidUsers = mkForce []; 
+    invalidUsers = lib.mkForce []; 
   };
 
 
