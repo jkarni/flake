1ee7d59263b287bfa872e42e3055d945b79156c8
mlyxshi <mail@mlyxshi.com>
2022-06-14 05:03:30 -0700

    test ovh

--- host/ovh/default.nix
+++ host/ovh/default.nix
@@ -2,7 +2,7 @@
 { config, pkgs, homeStateVersion, ... }: {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = {inherit homeStateVersion;};
+  home-manager.extraSpecialArgs = { inherit homeStateVersion; };
   home-manager.users.root = import ../../home/server.nix;
 
   imports = [
@@ -25,6 +25,19 @@
   services.nginx.enable = true;
   services.nginx.virtualHosts."ovh.mlyxshi.com" = {
     root = "/var/www/myhost.org";
+  };
+
+
+  services.samba.enable = true;
+  services.samba.shares = {
+    public =
+      {
+        path = "/srv/public";
+        "read only" = true;
+        browseable = "yes";
+        "guest ok" = "yes";
+        comment = "Public samba share.";
+      };
   };
 
   services.prometheus.exporters = {
