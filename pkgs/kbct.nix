a5ebb0abb88220f2c4427fece75a019846688002
mlyxshi <mail@mlyxshi.com>
2022-06-15 03:43:57 -0700

    test

--- pkgs/kbct.nix
+++ pkgs/kbct.nix
@@ -1,7 +1,11 @@
-{ pkgs, fetchurl, ... }: pkgs.appimageTools.wrapType1 {
+{ pkgs, fetchurl, ... }: pkgs.appimageTools.wrapType2 {
   name = "kbct";
   src = fetchurl {
     url = "https://github.com/samvel1024/kbct/releases/download/v0.1.0/kbct-x86_64.AppImage";
     sha256 = "sm2kMkYJi3VkSRtlBr5qIr24i9FAKC6e/xQrwG1KST0=";
   };
+  extraInstallCommands = ''
+    mkdir -p $out/etc/kbct
+    echo "test" > $out/etc/kbct/kbct.yml
+  '';
 }
