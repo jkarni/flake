0e2cb9cea82540cc857d2ea6fb2e08f7c960db9c
mlyxshi <mail@mlyxshi.com>
2022-06-20 08:43:35 -0700

    refactor overlay

--- overlay/Firefox.nix
+++ overlay/Firefox.nix
@@ -2,18 +2,19 @@
 # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/browsers/firefox/wrapper.nix
 # https://nixos.wiki/wiki/Firefox
 # https://github.com/nix-community/home-manager/blob/master/modules/programs/firefox.nix#blob-path
-{ lib, pkgs, ... }:
+{ lib, pkgs }:
 
 final: prev: {
   firefox =
-    if lib.optionals pkgs.stdenv.isLinux
-    then
+    if pkgs.stdenv.isLinux
+    then # Linux
       prev.wrapFirefox prev.firefox-unwrapped
         {
           forceWayland = true;
           # https://github.com/mozilla/policy-templates#enterprisepoliciesenabled
           extraPolicies = import ../config/firefox/policy.nix;
         }
-    else prev.callPackage ../pkgs/firefox-darwin { };
+    else # Darwin
+      prev.callPackage ../pkgs/firefox-darwin { };
 
 }
--- overlay/default.nix
+++ overlay/default.nix
@@ -1,8 +1,8 @@
-{neovim-nightly,zsh-tab-title,...}:{
+{ lib, pkgs, neovim-nightly, zsh-tab-title, ... }: {
   nixpkgs.overlays = [
     (import ./AppleFont.nix)
-    (import ./Zsh.nix {inherit zsh-tab-title;})
-    (import ./Neovim.nix {inherit neovim-nightly;})
-    (import ./Firefox.nix)
+    (import ./Zsh.nix { inherit zsh-tab-title; })
+    (import ./Neovim.nix { inherit neovim-nightly; })
+    (import ./Firefox.nix { inherit lib pkgs; })
   ];
 }
