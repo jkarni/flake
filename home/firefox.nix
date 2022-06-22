c5fc78876ecf90ad86a9d14f11cabb67f8d22156
mlyxshi <mail@mlyxshi.com>
2022-06-22 05:15:55 -0700

    configure firefox without hm module

--- home/firefox.nix
+++ home/firefox.nix
@@ -9,32 +9,32 @@ let
       ".mozilla/firefox"
     else
       "Library/Application Support/Firefox";
+
+
+  # See details: overlay/Firefox.nix
+  Firefox =
+    if pkgs.stdenv.isLinux then # Linux
+    # delete original  lib/firefox/defaults
+    # change to github:xiaoxiaoflood/firefox-scripts/
+      pkgs.firefox.overrideAttrs
+        (old: {
+          buildCommand = old.buildCommand + ''
+            rm -rf "$out/lib/firefox/defaults/"
+            mkdir -p  "$out/lib/firefox/defaults/pref/"
+
+            cat ${configPrefs} > "$out/lib/firefox/defaults/pref/config-prefs.js"
+            cat ${configJs} > "$out/lib/firefox/config.js"
+          '';
+        })
+    else # Darwin
+      pkgs.firefox;
 in
 
 {
 
-  programs.firefox = {
-    enable = true;
-    # See details: overlay/Firefox.nix
-    package =
-      if pkgs.stdenv.isLinux
-      then # Linux
-
-      # delete original  lib/firefox/defaults
-      # change to github:xiaoxiaoflood/firefox-scripts/
-        pkgs.firefox.overrideAttrs
-          (old: {
-            buildCommand = old.buildCommand + ''
-              rm -rf "$out/lib/firefox/defaults/"
-              mkdir -p  "$out/lib/firefox/defaults/pref/"
-
-              cat ${configPrefs} > "$out/lib/firefox/defaults/pref/config-prefs.js"
-              cat ${configJs} > "$out/lib/firefox/config.js"
-            '';
-          })
-      else # Darwin
-        pkgs.firefox;
-  };
+  home.packages = [
+    Firefox
+  ];
 
 
   home.file = {
