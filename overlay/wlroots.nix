68eabc6ccb42508a2de0e186013ae3a3c73561ec
mlyxshi <mail@mlyxshi.com>
2022-06-24 03:12:25 -0700

    wlroot nightly

--- overlay/wlroots.nix
+++ overlay/wlroots.nix
@@ -1,7 +1,7 @@
-{ fetchFromGitLab  }:
+{ fetchFromGitLab }:
 
 final: prev: {
-  wlroots = prev.wlroots.overrideAttrs (finalAttrs: previousAttrs: {
+  wlroots = prev.wlroots.overrideAttrs (finalAttrs: previousAttrs: rec {
     version = "master";
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
