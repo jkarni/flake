7e214d89d696b39b4628d589e538199b933d7f81
mlyxshi <mail@mlyxshi.com>
2022-06-25 22:21:27 -0700

    test

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,8 +1,8 @@
-{ lib, pkgs, neovim-nightly, ... }: {
+{ pkgs, lib, ... }@args: {
   nixpkgs.overlays = [
     (import ./AppleFont.nix)
     (import ./Anime4k.nix)
-    (import ./Neovim.nix { inherit neovim-nightly; })
+    (import ./Neovim.nix { inherit (args) neovim-nightly; })
     (import ./Firefox.nix { inherit (pkgs) stdenv; })
   ];
 }
