a8b0f2b4cd3c15df0386a9564a2b3b1740180d83
mlyxshi <mail@mlyxshi.com>
2022-07-05 05:30:15 -0700

    rclone module

--- host/oracle/jp4.nix
+++ host/oracle/jp4.nix
@@ -9,6 +9,6 @@
   ];
 
   # manually copy rclone config to /var/lib/qbittorrent-nox/rclone/rclone.conf  <-- GDtoken will refresh itself
-  services.bt.enable =ture;
+  services.bt.enable = true;
 
 }
