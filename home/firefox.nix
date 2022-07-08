43b3e4c4c3be23d0f373cf2f0071759a9b89be06
mlyxshi <mail@mlyxshi.com>
2022-07-08 09:30:25 -0700

    woodruffw/ff2mpv

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,29 +1,49 @@
-{
-  pkgs,
-  lib,
-  config,
-  osConfig,
-  ...
-}: let
-  firefoxConfigPath =
+{ pkgs
+, lib
+, config
+, osConfig
+, ...
+}:
+let
+  FirefoxConfigPath =
     if pkgs.stdenv.isLinux
     then ".mozilla/firefox"
     else "Library/Application Support/Firefox";
 
-  Firefox =
+  NativeMessagingHostsPath =
+    if pkgs.stdenv.isLinux
+    then ".mozilla/native-messaging-hosts"
+    else "Library/Application Support/Mozilla/NativeMessagingHosts";
+
+  FirefoxPackage =
     if pkgs.stdenv.isLinux
     then pkgs.firefox-nightly-bin
     else pkgs.firefox-nightly-bin-darwin;
-in {
+
+  ff-mpv = pkgs.writeScript "ff-mpv"  (builtins.readFile ../config/firefox/ff-mpv.py);
+
+in
+{
   home.packages = [
-    Firefox
+    FirefoxPackage
   ];
 
   #  https://support.mozilla.org/en-US/kb/understanding-depth-profile-installation
   #  Linux firefox wrapper set MOZ_LEGACY_PROFILES=1 by default
   #  Under macOS, we need to set System-level environment variable MOZ_LEGACY_PROFILES=1 by launchctl setenv, See os/darwin/default.nix
   home.file = {
-    "${firefoxConfigPath}/profiles.ini".source = config.lib.file.mkOutOfStoreSymlink ../config/firefox/profile/profiles.ini;
-    "${firefoxConfigPath}/default/chrome".source = config.lib.file.mkOutOfStoreSymlink ../config/firefox/profile/default/chrome;
+    "${FirefoxConfigPath}/profiles.ini".source = config.lib.file.mkOutOfStoreSymlink ../config/firefox/profile/profiles.ini;
+    "${FirefoxConfigPath}/default/chrome".source = config.lib.file.mkOutOfStoreSymlink ../config/firefox/profile/default/chrome;
+
+    # woodruffw/ff2mpv 
+    "${NativeMessagingHostsPath}/ff2mpv.json".text = ''
+      {
+        "name": "ff2mpv",
+        "description": "ff2mpv's external manifest",
+        "path": "${ff-mpv}",
+        "type": "stdio",
+        "allowed_extensions": ["ff2mpv@yossarian.net"]
+      }
+    '';
   };
 }
