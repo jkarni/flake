2233b3be14cdeae3a4f4a18b8fd889a7d6ca3175
mlyxshi <mail@mlyxshi.com>
2022-07-27 20:08:59 -0700

    us1

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -1,9 +1,25 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   imports = [
     ./default.nix
   ];
+
+
+  environment.systemPackages = [
+    nodejs
+    ytb-dlp
+  ];
+
+  # systemd.services."UnblockNeteaseMusic" = {
+  #   description = "UnblockNeteaseMusic Daemon";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+
+  #   serviceConfig = {
+  #     Restart = "always";
+  #     ExecStart = "${pkgs.shadowsocks-rust}/bin/ssserver -c ${config.sops.secrets.shadowsocks-config.path}";
+  #   };
+  # };
 }
