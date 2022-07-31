f4cdad6c6535c340d0855ef8286ad0fea0c9b486
mlyxshi <mail@mlyxshi.com>
2022-07-31 11:49:05 -0700

    test

--- host/oracle/jp4.nix
+++ host/oracle/jp4.nix
@@ -1,11 +1,31 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   imports = [
     ./default.nix
   ];
 
-  services.qbittorrent-nox.enable = true;
+
+  services = {
+    qbittorrent-nox.enable = true;
+
+    libreddit = {
+      address = "127.0.0.1";
+      enable = true;
+      port = 8081;
+    };
+
+    traefik = {
+      dynamicConfigOptions.http = {
+        routers.libreddit = {
+          rule = "Host(`reddit.mlyxshi.com`)";
+          service = "libreddit";
+        };
+        services.libreddit.loadBalancer.servers = [{ url = "http://${config.services.libreddit.address}:${builtins.toString config.services.libreddit.port}"; }];
+      };
+    };
+
+
+  };
 }
--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -10,23 +10,5 @@
 
   services = {
     unblock-netease-music.enable = true;
-
-    libreddit = {
-      address = "127.0.0.1";
-      enable = true;
-      port = 8081;
-    };
-
-    traefik = {
-      dynamicConfigOptions.http = {
-        routers.libreddit = {
-          rule = "Host(`reddit.mlyxshi.com`)";
-          service = "libreddit";
-        };
-        services.libreddit.loadBalancer.servers = [{ url = "http://${config.services.libreddit.address}:${builtins.toString config.services.libreddit.port}"; }];
-      };
-    };
-
-
   };
 }
