462b2f0ba18c15ce49f267ce9be659b26d1143fc
mlyxshi <mail@mlyxshi.com>
2022-07-30 17:44:11 -0700

    test

--- host/oracle/sw.nix
+++ host/oracle/sw.nix
@@ -1,13 +1,14 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
   imports = [
     ./default.nix
   ];
 
-
+  # mkdir -p /download/sonarr/config
+  # mkdir -p /download/jackett/config
+  # mkdir -p /download/jackett/downloads
   virtualisation.oci-containers.containers = {
     # Port 9117
     "jackett" = {
@@ -20,6 +21,24 @@
         "--network=host"
       ];
     };
+
+    # Port 8989
+    sonarr = {
+      image = "linuxserver/sonarr";
+      volumes = [
+        "/download/sonarr/config:/config"
+        "/download/sonarr:/data"
+      ];
+      environment = {
+        "PUID" = "0";
+        "PGID" = "0";
+      };
+      extraOptions = [
+        "--network=host"
+      ];
+    };
+
+
 
   };
 }
