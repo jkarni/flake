c2c60a585c4429f67970eaf996118fd242a361c2
mlyxshi <mail@mlyxshi.com>
2022-07-30 18:22:32 -0700

    test

--- host/oracle/sw.nix
+++ host/oracle/sw.nix
@@ -6,16 +6,18 @@
     ./default.nix
   ];
 
-  # mkdir -p /download/sonarr/config
+
   # mkdir -p /download/jackett/config
   # mkdir -p /download/jackett/downloads
+  # mkdir -p /download/sonarr/config
+  # mkdir -p /download/sonarr/downloads
   virtualisation.oci-containers.containers = {
     # Port 9117
     "jackett" = {
       image = "linuxserver/jackett";
       volumes = [
         "/download/jackett/config:/config"
-        "/download/jackett/downloads:/downloads"
+        "/download/jackett/downloads:/downloads"  #torrent
       ];
       extraOptions = [
         "--network=host"
@@ -27,7 +29,7 @@
       image = "linuxserver/sonarr";
       volumes = [
         "/download/sonarr/config:/config"
-        "/download/sonarr:/anime"
+        "/download/sonarr/downloads:/tv"  # real download file
       ];
       environment = {
         "PUID" = "0";
