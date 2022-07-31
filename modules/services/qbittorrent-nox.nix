8304b28bdcce6c4bfd0ed119c84cbe5cb6914175
mlyxshi <mail@mlyxshi.com>
2022-07-30 22:34:59 -0700

    test

--- modules/services/qbittorrent-nox.nix
+++ modules/services/qbittorrent-nox.nix
@@ -16,22 +16,16 @@
     torrent_size=$4
     file_hash=$5
     torrent_category=$6
-
     qb_web_url="localhost:8080"
     rclone_dest="googleshare:Download"
     log_dir="${qbConfigDir}/qBLog"
     rclone_parallel="32"
-
     # For upload_category, after download, upload to googledrive but do not auto delete(important resource, PT share ratio requirement)
     upload_category="upload"
-
-
     if [ ! -d $log_dir ]
     then
       mkdir -p $log_dir
     fi
-
-
     check_category(){
         if [ "$torrent_category" == $upload_category ]
         then
@@ -40,23 +34,16 @@
           echo "[$(date '+%Y-%m-%d %H:%M:%S')] Not Upload Category" >> $log_dir/qb.log
         fi
     }
-
     rclone_copy(){
         if [ -f "$content_dir" ]
         then
-          # 单集追番优化，RSS Feed Download Rules -> Save to a Different Directory(example: /var/lib/qbittorrent-nox/qBittorrent/downloads/Lycoris Recoil)
-          # https://www.shellscript.sh/tips/pattern-substitution/
-          downloadPathPrefix="/var/lib/qbittorrent-nox/qBittorrent/downloads/"
-                                                                                                                                                           # escape
-          ${pkgs.rclone}/bin/rclone --config ${config.sops.secrets.rclone-config.path} -v copy --log-file  $log_dir/rclone.log "$content_dir" $rclone_dest/"''${content_dir/$downloadPathPrefix}"
+          ${pkgs.rclone}/bin/rclone --config ${config.sops.secrets.rclone-config.path} -v copy --log-file  $log_dir/rclone.log "$content_dir" $rclone_dest
         elif [ -d "$content_dir" ]
         then
           ${pkgs.rclone}/bin/rclone --config ${config.sops.secrets.rclone-config.path} -v copy --transfers $rclone_parallel --log-file $log_dir/rclone.log "$content_dir" $rclone_dest/"$torrent_name"
         fi
-
         echo -e "-------------------------------------------------------------\n" >> $log_dir/rclone.log
     }
-
     qb_del(){
         if [ $torrent_category == $upload_category ]
         then
@@ -66,7 +53,6 @@
           echo "[$(date '+%Y-%m-%d %H:%M:%S')] Auto-delete Success" >> $log_dir/qb.log
         fi
     }
-
     telegram(){
         TOKEN=$(cat ${config.sops.secrets.tg-notify-token.path})
         CHAT_ID=$(cat ${config.sops.secrets.tg-userid.path})
@@ -75,22 +61,17 @@
         ${pkgs.curl}/bin/curl -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
         echo "[$(date '+%Y-%m-%d %H:%M:%S')] Notification Success" >> $log_dir/qb.log
     }
-
-
     echo "Torrent Name：$torrent_name" >> $log_dir/qb.log
     echo "Content Path：$content_dir" >> $log_dir/qb.log
     echo "File Number：$files_num" >> $log_dir/qb.log
     echo "Size：$torrent_size Bytes" >> $log_dir/qb.log
     echo "HASH: $file_hash" >> $log_dir/qb.log
     echo "Category: $torrent_category" >> $log_dir/qb.log
-
     check_category
     rclone_copy
     qb_del
     telegram
-
     echo -e "-------------------------------------------------------------\n" >> $log_dir/qb.log
-
   '';
 in {
   options = {
