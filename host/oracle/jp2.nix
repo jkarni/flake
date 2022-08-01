fa556bec31ab7a05662f7fa03259c6fb5defb4d5
mlyxshi <mail@mlyxshi.com>
2022-08-01 14:41:59 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -8,14 +8,17 @@
 
   services.rss-telegram.enable = true;
 
-  virtualisation.oci-containers.containers = {
-    #podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili
-    "unblocknetease" = {
-      image = "pan93412/unblock-netease-music-enhanced";
-      ports = [
-        "8080:8080"
-      ];
-      entrypoint = "node app.js -e https://music.163.com -o ytdlp bilibili";
+
+  # podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili
+  # can not use oci-containers directly, virtualisation.oci-containers.containers."xx".cmd will parse to "-e https://music.163.com" "-o ytdlp bilibili", UnblockNeteaseserver do not support this format.
+
+  systemd.services.unblock-netease-music = {
+    description = "unblock-netease-music";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = "podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
     };
   };
 }
