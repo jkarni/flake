c041fe32c5bb2da3f5b5a2600a1db68969b14df8
mlyxshi <mail@mlyxshi.com>
2022-08-01 13:10:14 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -17,87 +17,87 @@
     unblock-netease-music.enable = true;
   };
 
-  systemd.services.traefik.serviceConfig.EnvironmentFile = config.sops.secrets.traefik-cloudflare-env.path;
+  # systemd.services.traefik.serviceConfig.EnvironmentFile = config.sops.secrets.traefik-cloudflare-env.path;
 
-  services.traefik = {
-    enable = true;
+  # services.traefik = {
+  #   enable = true;
 
-    dynamicConfigOptions = {
-      middlewares.compress.compress = { };
-      tls.options.default = {
-        minVersion = "VersionTLS12";
-        sniStrict = true;
-      };
-      http.routers = {
+  #   dynamicConfigOptions = {
+  #     middlewares.compress.compress = { };
+  #     tls.options.default = {
+  #       minVersion = "VersionTLS12";
+  #       sniStrict = true;
+  #     };
+  #     http.routers = {
 
-        libreddit.rule = "Host(`reddit.mlyxshi.com`)";
-        libreddit.service = "libreddit";
+  #       libreddit.rule = "Host(`reddit.mlyxshi.com`)";
+  #       libreddit.service = "libreddit";
 
-        nitter.rule = "Host(`twitter.mlyxshi.com`)";
-        nitter.service = "nitter";
+  #       nitter.rule = "Host(`twitter.mlyxshi.com`)";
+  #       nitter.service = "nitter";
 
-        youtube.rule = "Host(`youtube.mlyxshi.com`)";
-        youtube.service = "youtube";
-      };
+  #       youtube.rule = "Host(`youtube.mlyxshi.com`)";
+  #       youtube.service = "youtube";
+  #     };
 
-      http.services = {
-        libreddit.loadBalancer.servers = [{ url = "http://localhost:8082"; }];
-        nitter.loadBalancer.servers = [{ url = "http://localhost:8083"; }];
-        youtube.loadBalancer.servers = [{ url = "http://localhost:8084"; }];
-      };
-    };
+  #     http.services = {
+  #       libreddit.loadBalancer.servers = [{ url = "http://localhost:8082"; }];
+  #       nitter.loadBalancer.servers = [{ url = "http://localhost:8083"; }];
+  #       youtube.loadBalancer.servers = [{ url = "http://localhost:8084"; }];
+  #     };
+  #   };
 
-    staticConfigOptions = {
-      certificatesResolvers.letsencrypt.acme = {
-        dnsChallenge.provider = "cloudflare";
-        email = "mlyxdev@gmail.com";
-        storage = "${config.services.traefik.dataDir}/acme.json"; # "/var/lib/traefik/acme.json"
-      };
+  #   staticConfigOptions = {
+  #     certificatesResolvers.letsencrypt.acme = {
+  #       dnsChallenge.provider = "cloudflare";
+  #       email = "mlyxdev@gmail.com";
+  #       storage = "${config.services.traefik.dataDir}/acme.json"; # "/var/lib/traefik/acme.json"
+  #     };
 
-      # api.dashboard = true;
-      # api.insecure = true;
+  #     # api.dashboard = true;
+  #     # api.insecure = true;
 
-      entryPoints = {
-        web = {
-          address = ":80";
-          http.redirections.entryPoint.to = "websecure";
-        };
-        websecure = {
-          address = ":443";
-          http.tls.certResolver = "letsencrypt";
-        };
-      };
-    };
-  };
-
-
+  #     entryPoints = {
+  #       web = {
+  #         address = ":80";
+  #         http.redirections.entryPoint.to = "websecure";
+  #       };
+  #       websecure = {
+  #         address = ":443";
+  #         http.tls.certResolver = "letsencrypt";
+  #       };
+  #     };
+  #   };
+  # };
 
 
-  services.libreddit = {
-    enable = true;
-    address = "127.0.0.1";
-    port = 8082;
-  };
 
-  services.nitter = {
-    enable = true;
-    preferences = {
-      replaceTwitter = config.services.nitter.server.hostname;
-      theme = "Auto";
-    };
-    server = {
-      address = "127.0.0.1";
-      https = true;
-      hostname = "twitter.mlyxshi.com";
-      port = 8083;
-    };
-  };
 
-  services.invidious = {
-    enable = true;
-    domain = "youtube.mlyxshi.com";
-    port = 8084;
-  };
+  # services.libreddit = {
+  #   enable = true;
+  #   address = "127.0.0.1";
+  #   port = 8082;
+  # };
+
+  # services.nitter = {
+  #   enable = true;
+  #   preferences = {
+  #     replaceTwitter = config.services.nitter.server.hostname;
+  #     theme = "Auto";
+  #   };
+  #   server = {
+  #     address = "127.0.0.1";
+  #     https = true;
+  #     hostname = "twitter.mlyxshi.com";
+  #     port = 8083;
+  #   };
+  # };
+
+  # services.invidious = {
+  #   enable = true;
+  #   domain = "youtube.mlyxshi.com";
+  #   port = 8084;
+  # };
 
 
 }
