9843a46c7cb7074e68ebda1d22dc7989206032f3
mlyxshi <mail@mlyxshi.com>
2022-07-31 22:55:38 -0700

    test

--- host/oracle/sw.nix
+++ host/oracle/sw.nix
@@ -25,50 +25,34 @@
 
 
   services.traefik.enable = true;
-  services.traefik.group = "docker";
-  services.traefik.staticConfigOptions = {
-    api.dashboard = true;
-    api.insecure = true;
-    entryPoints = {
-      web = {
-        address = ":80";
-      };
-      websecure = {
-        address = ":443";
+  services.traefik = {
+    dynamicConfigOptions = {
+      middlewares.compress.compress = { };
+      tls.options.default = {
+        minVersion = "VersionTLS12";
+        sniStrict = true;
       };
     };
-    # providers.docker = {
-    #   endpoint = "unix:///var/run/docker.sock";
-    #   exposedByDefault = false;
-    # };
-    # certificatesResolvers.cf.acme = {
-    #   email = "mlyxdev@gmail.com";
-    #   storage = "/srv/traefik/acme.json";
-    #   dnsChallenge = {
-    #     provider = "cloudflare";
-    #     resolvers = [ "1.1.1.1:53" "1.0.0.1:53" ];
-    #   };
-    # };
-  };
-
-  services.traefik.dynamicConfigOptions = {
-    # http.middlewares = {
-    #   compress.compress = {};
-    # };
-
-    # http.middlewares = {
-    #   web-redirect.redirectScheme.scheme = "https";
-    # };
-    tls.options.default = {
-      minVersion = "VersionTLS12";
-      cipherSuites = [
-        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-        "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
-        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
-        "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
-        "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
-      ];
+    enable = true;
+    staticConfigOptions = {
+      # certificatesResolvers.letsencrypt.acme = {
+      #   dnsChallenge.provider = "cloudflare";
+      #   email = "mlyxdev@gmail.com";
+      #   keyType = "EC256";
+      #   storage = "${config.services.traefik.dataDir}/acme.json";
+      # };
+      experimental.http3 = true;
+      entryPoints = {
+        http = {
+          address = ":80";
+          http.redirections.entryPoint.to = "https";
+        };
+        https = {
+          address = ":443";
+          http.tls.certResolver = "letsencrypt";
+          http3 = { };
+        };
+      };
     };
   };
 
