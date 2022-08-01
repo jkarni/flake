9b3abeb0ef2223e90d2c01b31c6a566641fb6f7f
mlyxshi <mail@mlyxshi.com>
2022-07-31 22:40:18 -0700

    test

--- host/oracle/sw.nix
+++ host/oracle/sw.nix
@@ -51,6 +51,26 @@
     # };
   };
 
+  services.traefik.dynamicConfigOptions = {
+    # http.middlewares = {
+    #   compress.compress = {};
+    # };
+
+    # http.middlewares = {
+    #   web-redirect.redirectScheme.scheme = "https";
+    # };
+    tls.options.default = {
+      minVersion = "VersionTLS12";
+      cipherSuites = [
+        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+        "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+        "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+        "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+      ];
+    };
+  };
 
 
   # https://reorx.com/blog/track-and-download-shows-automatically-with-sonarr
