59d2bdfc44171df57a48493a63977327cc293737
mlyxshi <mail@mlyxshi.com>
2022-08-01 14:02:31 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -11,7 +11,7 @@
   virtualisation.oci-containers.containers = {
     #podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili
     "unblocknetease" = {
-      image = "pan93412/unblock-netease-music-enhanced ";
+      image = "pan93412/unblock-netease-music-enhanced";
       ports = [
         "8080:8080"
       ];
