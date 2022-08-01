2354e5e7f4ee746b27c837f605cd1a91a8eec28c
mlyxshi <mail@mlyxshi.com>
2022-08-01 14:43:10 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -18,7 +18,7 @@
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-      ExecStart = "podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
+      ExecStart = "${pkgs.podman}/bin/podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
     };
   };
 }
