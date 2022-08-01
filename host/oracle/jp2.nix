b7c9632a6a4ba37e90001ef63a5790064ca1f622
mlyxshi <mail@mlyxshi.com>
2022-08-01 14:47:33 -0700

    test

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -10,7 +10,7 @@
 
 
   # podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili
-  # can not use oci-containers directly, virtualisation.oci-containers.containers."xx".cmd will parse to "-e https://music.163.com" "-o ytdlp bilibili", UnblockNeteaseserver do not support this format.
+  # can not use oci-containers directly, virtualisation.oci-containers.containers."xx".cmd will parse to "-e https://music.163.com" "-o ytdlp bilibili", UnblockNeteaseserver do not support quotation marks.
 
   systemd.services.unblock-netease-music = {
     description = "unblock-netease-music";
