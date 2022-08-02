fa523f024cd3c05278bade70ec5d2b3cb40cfbe8
mlyxshi <mail@mlyxshi.com>
2022-08-02 16:50:27 -0700

    test

--- modules/services/server-status/server/default.nix
+++ modules/services/server-status/server/default.nix
@@ -14,10 +14,13 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    system.activationScripts."installWebUI" = pkgs.lib.stringAfter [ "var" ] ''
-      [ ! -d /var/lib/ServerStatus/hotaru-theme ] && mkdir -p /var/lib/ServerStatus/; wget https://github.com/cokemine/hotaru_theme/releases/latest/download/hotaru-theme.zip; unzip /var/lib/ServerStatus/hotaru-theme.zip -d /var/lib/ServerStatus/web
-  
-    '';
+
+    # Please manually run this once
+    # mkdir -p /var/lib/ServerStatus/
+    # wget -P /var/lib/ServerStatus/ https://github.com/cokemine/hotaru_theme/releases/latest/download/hotaru-theme.zip
+    # unzip -d /var/lib/ServerStatus/ /var/lib/ServerStatus/hotaru-theme.zip 
+
+
 
     systemd.services.serverstatus-server = {
       description = "serverstatus-server";
