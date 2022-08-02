6b68aef0607e99ae3331ea365216591c21480406
mlyxshi <mail@mlyxshi.com>
2022-08-02 16:30:21 -0700

    test

--- modules/services/server-status/server/default.nix
+++ modules/services/server-status/server/default.nix
@@ -5,7 +5,6 @@
 }:
 let
   cfg = config.services.status-server;
-  #serverEnv =  pkgs.writeText "serverEnv.conf" (builtins.readFile ./serverEnv.conf);
   serverConfig = pkgs.writeText "serverConfig.json" (builtins.readFile ./serverConfig2.json);
 in
 {
@@ -19,19 +18,29 @@ in
       ServerStatus-Server
     ];
 
-    # EnvironmentFile=/usr/local/ServerStatus/server/config.conf
-    # ExecStart=/usr/local/ServerStatus/server/sergate --config=/usr/local/ServerStatus/server/config.json --web-dir=/usr/local/ServerStatus/web --port $PORT
-
-
     systemd.services.serverstatus-server = {
       description = "serverstatus-server";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        #EnvironmentFile = ${serverEnv};
         ExecStart = "${pkgs.ServerStatus-Server}/sergate  --config=${serverConfig} --web-dir=/var/lib/ServerStatus/hotaru-theme  --port 35601";
       };
+    };
+
+
+    # listen 80;
+
+    # server_name test.sharqa.com;
+
+    # root /home/linuxhint/www;
+
+    # index index.html;
+
+
+    services.nginx.enable = true;
+    services.nginx.virtualHosts."top.mlyxshi.com" = {
+      root = "/var/lib/ServerStatus/hotaru-theme";
     };
 
 
