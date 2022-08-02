2378d0c2a23b2cd7e4810ea6ec454aede5cb1687
mlyxshi <mail@mlyxshi.com>
2022-08-01 20:10:36 -0700

    test

--- host/oracle/jp4.nix
+++ host/oracle/jp4.nix
@@ -1,12 +1,30 @@
+{ config
+, pkgs
+, ...
+}:
+let
+  status-client = pkgs.writeScript "status-client" (''
+  
+  ''
+  + builtins.readFile ./status-client.py);
+
+in
 {
-  config,
-  pkgs,
-  ...
-}: {
   imports = [
     ./default.nix
   ];
 
   services.qbittorrent-nox.enable = true;
+
+
+  systemd.services.serverstatus-client = {
+    description = "serverstatus-client";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.python3}/bin/python ${status-client}";
+    };
+  };
 
 }
