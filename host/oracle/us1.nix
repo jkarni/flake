de01ac765698143e55318ddf3702cca9e6616b04
mlyxshi <mail@mlyxshi.com>
2022-08-03 12:01:42 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -66,7 +66,7 @@
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-      ExecStart = "${pkgs.podman}/bin/podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
+      ExecStart = "${pkgs.podman}/bin/podman run -p 8899:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
     };
   };
 }
