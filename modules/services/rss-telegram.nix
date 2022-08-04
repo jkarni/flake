6762a8170ade98c759e6e4faceb9ce9da0f32b71
mlyxshi <mail@mlyxshi.com>
2022-08-03 20:19:28 -0700

    test

--- modules/services/rss-telegram.nix
+++ modules/services/rss-telegram.nix
@@ -20,17 +20,17 @@ in
       [ ! -d ${RssConfigDir} ] && mkdir -p ${RssConfigDir}
     '';
 
-    system.activationScripts.SyncRssDNS = lib.stringAfter [ "var" ] ''
-      RED='\033[0;31m'
-      NOCOLOR='\033[0m'
+    # system.activationScripts.SyncRssDNS = lib.stringAfter [ "var" ] ''
+    #   RED='\033[0;31m'
+    #   NOCOLOR='\033[0m'
 
-      if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
-        echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
-        echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
-      else
-        ${pkgs.cloudflare-dns-sync} rss.mlyxshi.com
-      fi
-    '';
+    #   if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
+    #     echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
+    #     echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
+    #   else
+    #     ${pkgs.cloudflare-dns-sync} rss.mlyxshi.com
+    #   fi
+    # '';
 
     virtualisation.oci-containers.containers = {
       "rss-telegram" = {
