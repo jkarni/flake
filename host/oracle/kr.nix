1fd1646f41aefabbd8f19eb336f29b2b32c02726
mlyxshi <mail@mlyxshi.com>
2022-08-03 19:52:56 -0700

    test

--- host/oracle/kr.nix
+++ host/oracle/kr.nix
@@ -16,7 +16,7 @@
 
     if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
       echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
-      echo -e "$RED Please switch system again to use sops secrets $NOCOLOR"
+      echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
     else
       ${pkgs.cloudflare-dns-sync} change.mlyxshi.com
       ${pkgs.cloudflare-dns-sync} kms.mlyxshi.com
