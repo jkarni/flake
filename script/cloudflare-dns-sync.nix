e980ee8f514a55ab59ca9379bb1f62c35968c6af
mlyxshi <mail@mlyxshi.com>
2022-08-03 17:33:16 -0700

    test

--- script/cloudflare-dns-sync.nix
+++ script/cloudflare-dns-sync.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 let
   # arg1 = domain
-  cloudflare-dns-sync = pkgs.writeShellScript "cloudflare-dns-sync" ''
+  cloudflare-dns-sync = pkgs.writeShellScriptBin "cloudflare-dns-sync" ''
     domain=$1
 
     token=$(cat ${config.sops.secrets.cloudflare-dns-token.path})
