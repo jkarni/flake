1eec70c6b229a7a39f8be83c839a6a70f01e0149
mlyxshi <mail@mlyxshi.com>
2022-08-03 19:37:34 -0700

    test

--- script/cloudflare-dns-sync.nix
+++ script/cloudflare-dns-sync.nix
@@ -8,10 +8,10 @@ let
     NOCOLOR='\033[0m'
 
     if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
-      echo -e "${RED}Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time${NOCOLOR}"
+      echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time$NOCOLOR"
       # echo "Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time"
       # echo "Please rebuild system again to use sops secrets"
-      echo -e "${RED}Please rebuild system again to use sops secrets${NOCOLOR}"
+      echo -e "$RED Please rebuild system again to use sops secrets$NOCOLOR"
       exit 1;
     fi
 
@@ -27,7 +27,7 @@ let
     localIP=$(${pkgs.curl}/bin/curl --silent -4 ip.sb)
 
     if [ "$result" = "null" ]; then
-      echo -e "${YELLOW}$domain DNS Not Registered, Create DNS Record Now${NOCOLOR}"
+      echo -e "$YELLOW $domain DNS Not Registered, Create DNS Record Now$NOCOLOR"
       # echo "$domain DNS Not Registered, Create DNS Record Now"
       requestData=$(${pkgs.jq}/bin/jq --null-input --arg domain $domain --arg content $localIP '{"type":"A","name": $domain,"content": $content,"ttl":1,"proxied":false}')
       ${pkgs.curl}/bin/curl --silent -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
@@ -36,12 +36,12 @@ let
          --data "$requestData"
     else
       #echo "$domain DNS Registered"
-      echo -e "${GREEN}$domain DNS Registered${NOCOLOR}"
+      echo -e "$GREEN $domain DNS Registered$NOCOLOR"
       dnsID=$(echo $result | ${pkgs.jq}/bin/jq .id | tr -d '"')
       recordIP=$(echo $result | ${pkgs.jq}/bin/jq .content | tr -d '"')
       if [ $localIP != $recordIP ]; then
         #echo "IP Not Match, Update DNS Record"
-        echo -e "${YELLOW}IP Not Match, Update DNS Record${NOCOLOR}"
+        echo -e "$YELLOW IP Not Match, Update DNS Record$NOCOLOR"
         requestData=$(${pkgs.jq}/bin/jq --null-input --arg domain $domain --arg content $localIP '{"type":"A","name": $domain,"content": $content,"ttl":1,"proxied":false}')
         ${pkgs.curl}/bin/curl --silent -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsID" \
          -H "Content-Type: application/json" \
