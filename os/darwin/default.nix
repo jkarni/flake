9de74c8b13b9a44495e41d9ed66c5459cbe6455d
mlyxshi <mail@mlyxshi.com>
2022-08-04 14:52:43 -0700

    test

--- os/darwin/default.nix
+++ os/darwin/default.nix
@@ -1,8 +1,7 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
+{ pkgs
+, lib
+, config
+, ...
 }: {
   imports = [
     ./system.nix
@@ -27,17 +26,21 @@
   programs.ssh = {
     knownHosts = {
       github = {
-        hostNames = ["github.com"];
+        hostNames = [ "github.com" ];
         publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       };
     };
   };
 
   sops.defaultSopsFile = ../../modules/secrets/key.yaml;
-  # This is using an age key that is expected to already be in the filesystem
-  # We do not allow openssh login under MacOS
+
+
   # Under Linux, use sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # We do not allow openssh login under MacOS
+  # This is using an age key that is expected to already be in the filesystem
+  # ssh-to-age -private-key -i $HOME/.ssh/id_ed25519 -o "/Users/dominic/Library/Application Support/sops/age/keys.txt"
   sops.age.keyFile = "/Users/dominic/Library/Application Support/sops/age/keys.txt";
+
 
   sops.secrets.restic-password = {
     owner = "dominic";
