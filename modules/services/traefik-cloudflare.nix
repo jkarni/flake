5ff776bff69d4c2d3e74faf1aada1d8da83b85f9
mlyxshi <mail@mlyxshi.com>
2022-08-05 01:22:42 -0700

    test

--- modules/services/traefik-cloudflare.nix
+++ modules/services/traefik-cloudflare.nix
@@ -42,24 +42,14 @@ in
           };
           websecure = {
             address = ":443";
-            http.tls.certResolver = "zerossl";
+            http.tls.certResolver = "letsencrypt";
           };
         };
 
-        # Compare to letsencrypt, zerossl has a fancy dashboard
-        # Under NixOS context, it is very hard to hide zerossl kid and hmac 
-        # Anyway, please do not play prank on me! ~~Hope noboby see this~~
-        certificatesResolvers.zerossl.acme = {
-          caServer = "https://acme.zerossl.com/v2/DV90";
+        certificatesResolvers.letsencrypt.acme = {
+          dnsChallenge.provider = "cloudflare";
           email = "blackhole@mlyxshi.com";
-          storage = config.services.traefik.dataDir + "/acme.json";
-          dnsChallenge = {
-            provider = "cloudflare";
-          };
-          eab = {
-            kid = "vNX1_fYB1JNTdjDJyPxdyQ";
-            hmacEncoded = "aUiEz8TfxcZAW7FxjeHP8T7sG83PnR04Brcv_orQ_DZS1EWhpKoJl5Rh72-Utb3tovwxTj3e0Lylx3DdBbiAxg";
-          };
+          storage = "${config.services.traefik.dataDir}/acme.json"; # "/var/lib/traefik/acme.json"
         };
 
       }; # staticConfigOptions
