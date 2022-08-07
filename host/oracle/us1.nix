8d495bf4815cddb3e2e7031e0ab8ff9448c60491
mlyxshi <mail@mlyxshi.com>
2022-08-07 14:34:43 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -8,13 +8,7 @@
   ];
 
   services.status-client.enable = true;
-
-
-
-  # libreddit.loadBalancer.servers = [{ url = "http://localhost:8082"; }];
-  # nitter.loadBalancer.servers = [{ url = "http://localhost:8083"; }];
-  # youtube.loadBalancer.servers = [{ url = "http://localhost:8084"; }];
-
+  services.unblock-netease-music.enable = true;
 
   services.libreddit = {
     enable = true;
@@ -43,22 +37,7 @@
   };
 
 
-
-
-  # podman run -p 8080:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili
-  # can not use oci-containers directly, virtualisation.oci-containers.containers."xx".cmd will parse to "-e https://music.163.com" "-o ytdlp bilibili", UnblockNeteaseserver do not support quotation marks.
-
-  # systemd.services.unblock-netease-music = {
-  #   description = "unblock-netease-music";
-  #   after = [ "network.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.podman}/bin/podman run --name='unblock-netease-music' -p 8899:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
-  #   };
-  # };
-
-  sops.secrets.cloudflared-tunnel-env = { };
+  sops.secrets.cloudflared-tunnel-us-env = { };
   systemd.services.cloudflared = {
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" "systemd-resolved.service" ];
@@ -67,9 +46,8 @@
         ${pkgs.bash}/bin/bash -c "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token=$TOKEN"
       '';
       # Restart = "always";
-      EnvironmentFile = "/run/secrets/cloudflared-tunnel-env";
+      EnvironmentFile = config.sops.secrets.cloudflared-tunnel-us-env.path;
     };
   };
-
 
 }
