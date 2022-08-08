58429db8dbe11956e9a18e9351a5af18ec5db8a9
mlyxshi <mail@mlyxshi.com>
2022-08-07 22:35:41 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -76,7 +76,6 @@
   # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/misc/nitter.nix#L350
   # Disable old redis config way
   services.nitter.redisCreateLocally = false;
-  services.nitter.cache.redisPort = 6379;
   services.redis.servers.nitter = {
     enable = true;
     port = 6379;
