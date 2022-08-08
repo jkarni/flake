3f3a696d36df4c82b554c076a4c34581b03357a3
mlyxshi <mail@mlyxshi.com>
2022-08-08 12:14:21 -0700

    test

--- modules/services/unblock-netease-music.nix
+++ modules/services/unblock-netease-music.nix
@@ -20,6 +20,7 @@ in
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
+        ExecStartPre = "podman rm -f unblock-netease-music || true";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name='unblock-netease-music' -p 8899:8080 pan93412/unblock-netease-music-enhanced -e https://music.163.com -o ytdlp bilibili";
       };
     };
