c7359d2eb79392fec367c19db4cae7820736a76b
mlyxshi <mail@mlyxshi.com>
2022-08-10 13:13:15 -0700

    refactor

---
+++ host/test/default.nix
@@ -0,0 +1,26 @@
+{ config, ... }: {
+  imports = [
+    ./hardware.nix
+    ../../os/nixos
+  ];
+
+
+  networking = {
+    useNetworkd = true;
+    useDHCP = false;
+    firewall.enable = false;
+  };
+
+  # Workaround for fixing timeout issue
+  # manually reboot once
+  systemd.network.wait-online.anyInterface = true;
+
+  systemd.network.networks = {
+    dhcp = {
+      name = "ens4";
+      DHCP = "yes";
+    };
+  };
+
+  # networking.nameservers = ["1.1.1.1"];
+}
---
+++ host/test/hardware.nix
@@ -0,0 +1,19 @@
+{ config
+, lib
+, pkgs
+, modulesPath
+, ...
+}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.loader.grub.device = "/dev/vda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+
+  fileSystems."/" = {
+    device = "/dev/vda2";
+    fsType = "ext4";
+  };
+}
