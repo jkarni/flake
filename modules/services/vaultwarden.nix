5c64b7e581f0540a687eb251d8e3d14021929419
mlyxshi <mail@mlyxshi.com>
2022-08-14 21:26:35 -0700

    test

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ...}: {
+{ config, pkgs, lib, ... }: {
 
   sops.secrets.vaultwarden-domain = { };
   sops.secrets.vaultwarden-env = { };
@@ -11,21 +11,26 @@
     "vaultwarden" = {
       image = "vaultwarden/server";
       volumes = [ "/var/lib/vaultwarden:/data" ];
-  
-      extraOptions = [
-        "--label"
-        "traefik.enable=true"
-
-        "--label"
-        "traefik.http.routers.websecure-vaultwarden.rule=Host(`$DOMAIN`)"
-        "--label"
-        "traefik.http.routers.websecure-vaultwarden.entrypoints=websecure"
-      ];
     };
 
   };
 
 
   systemd.services.podman-vaultwarden.serviceConfig.EnvironmentFile = config.sops.secrets.vaultwarden-env.path;
+  systemd.services.podman-vaultwarden.serviceConfig.ExecStart = lib.mkForce (pkgs.writeShellScript "podman-vaultwarden-start" ''
+    set -e
+    exec podman run \
+      --rm \
+      --name='vaultwarden' \
+      --log-driver=journald \
+      -v '/var/lib/vaultwarden:/data' \
+      '--label' \
+      'traefik.enable=true' \
+      '--label' \
+      "traefik.http.routers.websecure-vaultwarden.rule=Host(`$DOMAIN`)" \
+      '--label' \
+      'traefik.http.routers.websecure-vaultwarden.entrypoints=websecure' \
+      vaultwarden/server
+  '');
 
 }
