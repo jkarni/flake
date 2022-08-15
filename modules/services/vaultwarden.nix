32f53975794af238d482ddc1b39b54c79b78104b
mlyxshi <mail@mlyxshi.com>
2022-08-14 21:15:38 -0700

    test

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -1,13 +1,7 @@
 { config, pkgs, lib, ...}: {
 
   sops.secrets.vaultwarden-domain = { };
-
-  services.traefik.dynamicConfigOptions = {
-    http.routers = {
-      vaultwarden.rule = "{{ env `VAULTWARDEN_DOMAIN` }}";
-      vaultwarden.service = "vaultwarden@docker";
-    };
-  };
+  sops.secrets.vaultwarden-env = { };
 
   system.activationScripts.makeVaultwardenDir = lib.stringAfter [ "var" ] ''
     [ ! -d /var/lib/vaultwarden ] && mkdir -p /var/lib/vaultwarden
@@ -21,8 +15,17 @@
       extraOptions = [
         "--label"
         "traefik.enable=true"
+
+        "--label"
+        "traefik.http.routers.websecure-vaultwarden.rule=Host(`$DOMAIN`)"
+        "--label"
+        "traefik.http.routers.websecure-vaultwarden.entrypoints=websecure"
       ];
     };
 
   };
+
+
+  systemd.services.podman-vaultwarden.serviceConfig.EnvironmentFile = config.sops.secrets.vaultwarden-env.path;
+
 }
