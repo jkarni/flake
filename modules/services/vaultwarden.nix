bb013476b2b0bbbf974bc734f86bd9baf32a266a
mlyxshi <mail@mlyxshi.com>
2022-08-14 22:10:12 -0700

    test

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -3,6 +3,18 @@
   sops.secrets.vaultwarden-domain = { };
   sops.secrets.vaultwarden-env = { };
 
+  system.activationScripts.SyncVaultwardenDNS = lib.stringAfter [ "var" ] ''
+    RED='\033[0;31m'
+    NOCOLOR='\033[0m'
+    if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ] || [ ! -f ${config.sops.secrets.vaultwarden-domain.path} ]; then
+      echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
+      echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
+    else
+      ${pkgs.cloudflare-dns-sync} $(cat ${config.sops.secrets.vaultwarden-domain.path})
+    fi
+  '';
+
+
   system.activationScripts.makeVaultwardenDir = lib.stringAfter [ "var" ] ''
     [ ! -d /var/lib/vaultwarden ] && mkdir -p /var/lib/vaultwarden
   '';
@@ -12,10 +24,9 @@
       image = "vaultwarden/server";
       volumes = [ "/var/lib/vaultwarden:/data" ];
     };
-
   };
 
-
+  # hacky way to hide actual domain
   systemd.services.podman-vaultwarden.serviceConfig.EnvironmentFile = config.sops.secrets.vaultwarden-env.path;
   systemd.services.podman-vaultwarden.serviceConfig.ExecStart = lib.mkForce (pkgs.writeShellScript "podman-vaultwarden-start" ''
     set -e
