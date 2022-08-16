d4e51d38051767043c19eb46466b8f8a29de5683
mlyxshi <mail@mlyxshi.com>
2022-08-15 17:27:46 -0700

    refactor

--- host/oracle/kr.nix
+++ host/oracle/kr.nix
@@ -4,6 +4,7 @@
     ../../services/traefik-cloudflare.nix  #80,443
     ../../services/kms.nix 
     ../../services/change-detection.nix
+    ../../services/miniflux-silent.nix  # miniflux-silent, miniflux should deploy to different hosts
   ];
 }
 
---
+++ services/miniflux-silent.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, ...}: {
+
+  sops.secrets.miniflux-env = { };
+  sops.secrets.miniflux-db-env = { };
+
+  system.activationScripts.makeMinifluxDBDir = lib.stringAfter [ "var" ] ''
+    [ ! -d /var/lib/miniflux-db ] && mkdir -p /var/lib/miniflux-db
+  '';
+
+  system.activationScripts.SyncMinifluxDNS = lib.stringAfter [ "var" ] ''
+    RED='\033[0;31m'
+    NOCOLOR='\033[0m'
+    if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
+      echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
+      echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
+    else
+      ${pkgs.cloudflare-dns-sync} miniflux-silent.${config.networking.domain}
+    fi
+  '';
+
+  virtualisation.oci-containers.containers = {
+
+    "miniflux" = {
+      image = "miniflux/miniflux";
+      dependsOn = [ "miniflux-db" ];
+      environmentFiles = [ config.sops.secrets.miniflux-env.path ];
+      environment = {
+        INVIDIOUS_INSTANCE = "youtube.${config.networking.domain}";
+        CREATE_ADMIN = "1";
+        RUN_MIGRATIONS = "1";
+      };
+      extraOptions = [
+
+        "--label"
+        "traefik.enable=true"
+
+        "--label"
+        "traefik.http.routers.websecure-miniflux.rule=Host(`miniflux-silent.${config.networking.domain}`)"
+        "--label"
+        "traefik.http.routers.websecure-miniflux.entrypoints=websecure"
+      ];
+    };
+
+    "miniflux-db" = {
+      image = "postgres";
+      volumes = [
+        "/var/lib/miniflux-db:/var/lib/postgresql/data"
+      ];
+      environmentFiles = [ config.sops.secrets.miniflux-db-env.path ];
+    };
+
+  };
+
+}
