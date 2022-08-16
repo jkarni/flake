dd7620bf1da82bf316d76a19a7cb3e278ebaf0a0
mlyxshi <mail@mlyxshi.com>
2022-08-16 05:17:50 -0700

    test

--- services/server-status/server/default.nix
+++ services/server-status/server/default.nix
@@ -7,24 +7,11 @@ let
   serverConfig = pkgs.writeText "serverConfig.json" (builtins.readFile ./serverConfig.json);
 in
 {
-  system.activationScripts.SyncServerStatusDNS = lib.stringAfter [ "var" ] ''
-    RED='\033[0;31m'
-    NOCOLOR='\033[0m'
-
-    if [ ! -f ${config.sops.secrets.cloudflare-dns-token.path} ]; then
-      echo -e "$RED Sops-nix Known Limitations: https://github.com/Mic92/sops-nix#using-secrets-at-evaluation-time $NOCOLOR"
-      echo -e "$RED Please switch system again to use sops secrets and sync DNS $NOCOLOR"
-    else
-      ${pkgs.cloudflare-dns-sync} top.${config.networking.domain}
-    fi
-  '';
-
 
   systemd.services.serverstatus-server = {
     description = "serverstatus-server";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
-    # install WebUI once
     preStart = ''
       if [ ! -d /var/lib/ServerStatus/hotaru-theme/json ]
       then
@@ -32,6 +19,8 @@ in
         ${pkgs.wget}/bin/wget -P /var/lib/ServerStatus/ https://github.com/cokemine/hotaru_theme/releases/latest/download/hotaru-theme.zip
         ${pkgs.unzip}/bin/unzip -d /var/lib/ServerStatus/ /var/lib/ServerStatus/hotaru-theme.zip
       fi
+
+      ${pkgs.cloudflare-dns-sync} top.${config.networking.domain}
     '';
 
     serviceConfig = {
