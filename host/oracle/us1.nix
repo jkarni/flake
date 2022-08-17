d1e452250de7947d4284db74b841f88979e071f3
mlyxshi <mail@mlyxshi.com>
2022-08-16 18:14:26 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -2,7 +2,7 @@
   imports = [
     ./default.nix
     ../../services/traefik-cloudflare.nix #80,443
-    ../../services/libre.nix
+    #../../services/libre.nix
     ../../services/vaultwarden.nix
   ];
 }
