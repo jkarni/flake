4d632bc1d673df4b20c41eaf318dbe8fd849e283
mlyxshi <mail@mlyxshi.com>
2022-08-16 20:37:14 -0700

    test

--- services/nitter.nix
+++ services/nitter.nix
@@ -51,21 +51,21 @@ in
 {
 
   virtualisation.oci-containers.containers = {
-    # "nitter" = {
-    #   image = "quay.io/unixfox/nitter";
-    #   dependsOn = [ "nitter-db" ];
-    #   volumes = [
-    #     "/var/lib/nitter/nitter.conf:/src/nitter.conf"
-    #   ];
-    #   extraOptions = [
-    #     "--label"
-    #     "traefik.enable=true"
-    #     "--label"
-    #     "traefik.http.routers.websecure-nitter.rule=Host(`twitter.${config.networking.domain}`)"
-    #     "--label"
-    #     "traefik.http.routers.websecure-nitter.entrypoints=websecure"
-    #   ];
-    # };
+    "nitter" = {
+      image = "quay.io/unixfox/nitter";
+      dependsOn = [ "nitter-db" ];
+      volumes = [
+        "/var/lib/nitter/nitter.conf:/src/nitter.conf"
+      ];
+      extraOptions = [
+        "--label"
+        "traefik.enable=true"
+        "--label"
+        "traefik.http.routers.websecure-nitter.rule=Host(`twitter.${config.networking.domain}`)"
+        "--label"
+        "traefik.http.routers.websecure-nitter.entrypoints=websecure"
+      ];
+    };
 
     "nitter-db" = {
       image = "redis";
@@ -77,15 +77,17 @@ in
 
   };
 
+
+  # Fix Redis WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.
   boot.kernel.sysctl = {
     "vm.overcommit_memory" = "1";
   };
 
-  # systemd.services.podman-nitter.preStart = lib.mkAfter ''
-  #   [ ! -d /var/lib/nitter ] && mkdir -p /var/lib/nitter
-  #   cat ${NitterConfig} > /var/lib/nitter/nitter.conf
-  #   ${pkgs.cloudflare-dns-sync} twitter.${config.networking.domain}
-  # '';
+  systemd.services.podman-nitter.preStart = lib.mkAfter ''
+    [ ! -d /var/lib/nitter ] && mkdir -p /var/lib/nitter
+    cat ${NitterConfig} > /var/lib/nitter/nitter.conf
+    ${pkgs.cloudflare-dns-sync} twitter.${config.networking.domain}
+  '';
 
   # systemd.services.podman-nitter-db.preStart = lib.mkAfter ''
   #   [ ! -d /var/lib/nitter-db ] && mkdir -p /var/lib/nitter-db
