a48b0151a5f943dc26ffae638b814be60403a3dd
mlyxshi <mail@mlyxshi.com>
2022-08-16 17:34:16 -0700

    test

--- services/unblock-netease-music.nix
+++ services/unblock-netease-music.nix
@@ -1,10 +1,10 @@
 { config, pkgs, lib, ... }: {
 
   sops.secrets.unblock-netease-music-env = { };
+
   systemd.services.podman-unblock-netease-music.preStart = lib.mkAfter ''
     ${pkgs.cloudflare-dns-sync} netease.${config.networking.domain}
   '';
-
 
   systemd.services.podman-unblock-netease-music.serviceConfig.EnvironmentFile = config.sops.secrets.unblock-netease-music-env.path;
   systemd.services.podman-unblock-netease-music.serviceConfig.ExecStart = lib.mkForce (pkgs.writeShellScript "podman-unblock-netease-music-start" ''
@@ -15,13 +15,7 @@
       --log-driver=journald \
       -p "$PORT:8080" \
       '--net=host' \
-      pan93412/unblock-netease-music-enhanced \
-      '--strict' \
-      '-e' \
-      'https://music.163.com' \
-      '-o' \
-      'ytdlp' \
-      'bilibili'
+      pan93412/unblock-netease-music-enhanced --strict -e https://music.163.com -o ytdlp bilibili
   '');
 
 }
