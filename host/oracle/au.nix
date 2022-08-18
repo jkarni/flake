e08cd4f55d0be0610410653cf3104c80c83116e2
mlyxshi <mail@mlyxshi.com>
2022-08-17 21:16:20 -0700

    test

--- host/oracle/au.nix
+++ host/oracle/au.nix
@@ -4,4 +4,10 @@
     ../../services/traefik-cloudflare.nix #80,443
 
   ];
+
+  sops.secrets.miniflux-env = { };
+
+  system.activationScripts.makeDownloadDir = pkgs.lib.stringAfter [ "var" ] ''
+    cat ${config.sops.secrets.miniflux-env.path} > /tmp/test
+  '';
 }
