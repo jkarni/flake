9d827d6122da4eb1f91884cef31e903986db90dd
mlyxshi <mail@mlyxshi.com>
2022-08-20 20:43:04 -0700

    test

--- host/oracle/kr.nix
+++ host/oracle/kr.nix
@@ -2,6 +2,7 @@
   imports = [
     ./default.nix
     ../../services/traefik-cloudflare.nix #80,443
+    ../../services/server-status/server
     ../../services/kms.nix
     ../../services/change-detection.nix
     ../../services/miniflux-silent.nix # miniflux-silent, miniflux should deploy to different hosts
