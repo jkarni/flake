b1627eaa237c3a1fd0684e87a9eee2db448b0edc
mlyxshi <mlyxdev@gmail.com>
2022-08-23 16:48:14 -0700

    test

--- host/oracle/au.nix
+++ host/oracle/au.nix
@@ -2,28 +2,5 @@
   imports = [
     ./default.nix
     ../../services/traefik-cloudflare.nix #80,443
-
   ];
-
-
-  virtualisation.oci-containers.containers = {
-
-    "change-detection-io" = {
-      image = "dgtlmoon/changedetection.io";
-      volumes = [ "/var/lib/changeio:/datastore" ];
-      extraOptions = [
-        "--label"
-        "traefik.enable=true"
-      ];
-    };
-
-  };
-
-  # systemd.services.podman-change-detection-io.preStart = lib.mkAfter ''
-  #   mkdir -p /var/lib/changeio
-  # '';
-
-  systemd.services.podman-change-detection-io.serviceConfig.StateDirectory = "changeio";
-
-
 }
