812c4808d546ec5f3c7f5f985761f32046b49a51
mlyxshi <mlyxdev@gmail.com>
2022-08-23 16:56:26 -0700

    test

--- home/git.nix
+++ home/git.nix
@@ -13,6 +13,7 @@
 
     extraConfig = {
       init.defaultBranch = "main";
+    } // lib.optionalAttrs pkgs.stdenv.isDarwin {
       # ssh sighatures
       # https://blog.dbrgn.ch/2021/11/16/git-ssh-signatures/
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe";
