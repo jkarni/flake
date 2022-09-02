80fa3764f5909cb460039bedfb2992e55240f8fa
mlyxshi <mlyxdev@gmail.com>
2022-09-02 12:54:25 -0700

    test

--- services/unblock-netease-music.nix
+++ services/unblock-netease-music.nix
@@ -15,8 +15,13 @@
       --name='unblock-netease-music' \
       --log-driver=journald \
       '--net=host' \
+      '--label' 'io.containers.autoupdate=registry' \
       pan93412/unblock-netease-music-enhanced -p $PORT --strict -e https://music.163.com -o ytdlp bilibili
   '';
+
+  systemd.services.podman-unblock-netease-music.environment = {
+    PODMAN_SYSTEMD_UNIT = "%n";
+  };
 
   system.activationScripts.cloudflare-dns-sync-unblock-netease-music = {
     deps = [ "setupSecrets" ];
