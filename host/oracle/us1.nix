aeadde301ee48d22afdc44da3e80911ff3500505
mlyxshi <mlyxdev@gmail.com>
2022-10-12 05:32:25 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -6,4 +6,12 @@
     ../../services/libreddit.nix
     ../../services/invidious.nix
   ];
+
+
+  systemd.services.podman-auto-update = {
+      serviceConfig = {
+        ExecStart = "${pkgs.podman}/bin/podman auto-update";
+        ExecStartPost = "${pkgs.podman}/bin/podman image prune -f";
+      };
+  };
 }
