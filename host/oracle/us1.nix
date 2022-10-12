289cc661f7a5648b284c86fd7317e43aa21b7d3a
mlyxshi <mlyxdev@gmail.com>
2022-10-12 05:44:42 -0700

    test

--- host/oracle/us1.nix
+++ host/oracle/us1.nix
@@ -8,27 +8,14 @@
   ];
 
 
-  # systemd.services.podman-auto-update = {
-  #   wants = [ "network-online.target" ];
-  #   after = [ "network-online.target" ];
-  #   serviceConfig = {
-  #     Type="oneshot";
-  #     ExecStart = "${pkgs.podman}/bin/podman auto-update";
-  #     ExecStartPost = "${pkgs.podman}/bin/podman image prune -f";
-  #   };
-  #   wantedBy =[ "default.target" ];
-  # };
-
-  systemd.services.podman-auto-update = {
-      serviceConfig = {
-        ExecStart = [
-          "" # override original
-          "${pkgs.podman}/bin/podman auto-update"
-        ];
-        ExecStartPost = [
-          "" # override original
-          "${pkgs.podman}/bin/podman image prune -f"
-        ];
-      };
+  systemd.services.podman-auto-update2 = {
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    serviceConfig = {
+      Type="oneshot";
+      ExecStart = "${pkgs.podman}/bin/podman auto-update";
+      ExecStartPost = "${pkgs.podman}/bin/podman image prune -f";
     };
+    wantedBy =[ "default.target" ];
+  };
 }
