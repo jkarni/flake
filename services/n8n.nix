ddec6395dc52934330783cd0b3fbfc4846572f37
mlyxshi <mlyxdev@gmail.com>
2022-10-13 10:37:59 -0700

    test

---
+++ services/n8n.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, lib, ... }: {
+
+  virtualisation.oci-containers.containers = {
+    "n8n" = {
+      image = "n8nio/n8n";
+      volumes = [
+        "/var/lib/n8n:/home/node/.n8n"
+      ];
+      extraOptions = [
+        "--label"
+        "traefik.enable=true"
+
+        "--label"
+        "traefik.http.routers.kms.rule=Host(`n8n.${config.networking.domain}`)"
+        "--label"
+        "traefik.http.routers.kms.entrypoints=websecure"
+
+        "--label"
+        "io.containers.autoupdate=registry"
+      ];
+    };
+  };
+
+  systemd.services.podman-n8n.environment.PODMAN_SYSTEMD_UNIT = "%n";
+  systemd.services.podman-n8n.serviceConfig.StateDirectory = "n8n";
+
+  system.activationScripts.cloudflare-dns-sync-n8n = {
+    deps = [ "setupSecrets" ];
+    text = ''
+      ${pkgs.cloudflare-dns-sync} n8n.${config.networking.domain}
+    '';
+  };
+
+}
