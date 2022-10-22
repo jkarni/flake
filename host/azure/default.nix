39c14575e13ea8448b5d3e8e9438908cecfa4e65
mlyxshi <mlyxdev@gmail.com>
2022-10-22 04:22:34 -0700

    test

---
+++ host/azure/default.nix
@@ -0,0 +1,41 @@
+{
+  imports = [
+    ./hardware.nix
+    ../../os/nixos
+    ../../home/home-manager.nix
+
+    ../../script/cloudflare-dns-sync.nix
+    ../../services/server-status/client
+    ../../services/shadowsocks-rust.nix
+    ../../services/ssh-config.nix
+  ];
+
+  home-manager.users.root = import ../../home;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 3;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.tmpOnTmpfs = true;
+
+  virtualisation.podman.enable = true;
+  # unlike docker, to enable dns resolution between different containers, we need enable dnsname plugin under podman --> https://github.com/containers/dnsname 
+  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+
+  networking = {
+    useNetworkd = true;
+    useDHCP = false;
+    firewall.enable = false;
+  };
+
+  # Workaround for fixing timeout issue
+  # manually reboot once
+  systemd.network.wait-online.anyInterface = true;
+
+  systemd.network.networks = {
+    dhcp = {
+      name = "enp0s3";
+      DHCP = "yes";
+    };
+  };
+}
---
+++ host/azure/hardware.nix
@@ -0,0 +1,15 @@
+{ modulesPath, ... }: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/sda2";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/sda1";
+    fsType = "vfat";
+  };
+}
