f7cc756b2c1446b49eed0791c65bd89e07fa27d4
mlyxshi <mlyxdev@gmail.com>
2022-10-23 11:35:00 -0700

    test

--- os/darwin/activationScripts.nix
+++ os/darwin/activationScripts.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }: {
   system.activationScripts.postActivation.text = ''
+
+    # Update Firefox Dock item, https://github.com/kcrawford/dockutil/releases
+    /usr/local/bin/dockutil --remove Firefox --no-restart
+    /usr/local/bin/dockutil --add ${pkgs.firefox-bin-darwin}/Applications/Firefox.app  --after 'Visual Studio Code'  --no-restart
+    killall Dock
+    
     #  show upgrade diff
     ${pkgs.nix}/bin/nix store  diff-closures /run/current-system "$systemConfig"
   '';
