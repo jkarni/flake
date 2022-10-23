f56750fe9b571f1524ebf7b22da3d01480388512
mlyxshi <mlyxdev@gmail.com>
2022-10-23 10:05:28 -0700

    test

--- os/darwin/activationScripts.nix
+++ os/darwin/activationScripts.nix
@@ -1,20 +1,5 @@
-{ pkgs, ... }:
-let
-  extraPolicies = import ../../config/firefox/app/policy.nix;
-  wrapperPolicies = {
-    policies = {
-      DisableAppUpdate = true;
-    } // extraPolicies;
-  };
-  policiesJson = pkgs.writeText "policies.json" (builtins.toJSON wrapperPolicies);
-in
-{
-
+{ pkgs, ... }:{
   system.activationScripts.postActivation.text = ''
-    # add firefox(homebrew) policy
-    mkdir -p "/Applications/Firefox.app/Contents/Resources/distribution"
-    cat ${policiesJson} > "/Applications/Firefox.app/Contents/Resources/distribution/policies.json"
-
     #  show upgrade diff
     ${pkgs.nix}/bin/nix store  diff-closures /run/current-system "$systemConfig"
   '';
