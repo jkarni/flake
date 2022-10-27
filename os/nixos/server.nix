6c7b72561537d8832e24b1b1b790a6ac577bf15c
mlyxshi <mlyxdev@gmail.com>
2022-10-27 10:11:09 -0700

    test

--- os/nixos/server.nix
+++ os/nixos/server.nix
@@ -1,8 +1,11 @@
 { config, pkgs, lib, modulesPath, ... }: {
 
   imports = [
-    "${modulesPath}/profiles/minimal.nix"
     ./base.nix
     ./sops.nix
   ];
+
+  documentation.enable = false;
+  documentation.nixos.enable = false;
+  programs.command-not-found.enable = false;
 }
