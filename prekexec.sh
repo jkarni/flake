df6cae29b8256068ea63cd51a26295101aaf523b
mlyxshi <mlyxdev@gmail.com>
2022-10-28 18:00:18 -0700

    test

--- prekexec.sh
+++ prekexec.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # install  bzImage initrd.gz kexec-boot
-apt install -y jq wget kexec-tools
+apt install -y jq wget curl kexec-tools
 kexec_files=$(curl --silent "https://api.github.com/repos/mlyxshi/flake/releases/latest"| jq ."assets[].browser_download_url" | tr -d '"')
 
 for i in $kexec_files
