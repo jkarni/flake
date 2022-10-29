edc0c1d12e08faad68fc1930e5e5439a2100f837
mlyxshi <mlyxdev@gmail.com>
2022-10-29 13:58:34 -0700

    test

--- install-template.sh
+++ install-template.sh
@@ -3,6 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+
 KEY_URL=$1
 
 sfdisk /dev/sda <<EOT
@@ -24,7 +25,7 @@ mount /dev/sda1 /mnt/boot
 mkdir -p /mnt/var/lib/sops/
 curl -s "$KEY_URL" -o /mnt/var/lib/sops/age.key
 
-nixos-install --root /mnt --system YOUR_SYSTEM_CLOSURE \
+nixos-install --root /mnt --system $SYSTEM_CLOSURE \
 --no-channel-copy --no-root-passwd \
 --option trusted-public-keys "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" \
 --option substituters "https://cache.garnix.io" -v
